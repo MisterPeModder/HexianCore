7e1448e5d998a99628780186be75be9af1e6aa5e
MisterPeModder <yguaye44@gmail.com>
2017-09-05 05:49:44 +0200

    1.0.2: Vanilla clients can connect to Modded Servers with HexianCore installed

--- src/main/java/misterpemodder/hc/HexianCore.java
+++ src/main/java/misterpemodder/hc/HexianCore.java
@@ -20,7 +20,13 @@ package misterpemodder.hc;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.Instance;
 
-@Mod(modid = HCRefs.MOD_ID, name = HCRefs.MOD_NAME, version = HCRefs.MOD_VERSION, acceptedMinecraftVersions = HCRefs.ACCEPTED_MC_VERSIONS)
+@Mod(
+		modid = HCRefs.MOD_ID,
+		name = HCRefs.MOD_NAME,
+		version = HCRefs.MOD_VERSION,
+		acceptedMinecraftVersions = HCRefs.ACCEPTED_MC_VERSIONS,
+		acceptableRemoteVersions = "*"
+	)
 public class HexianCore {
 	
 	@Instance(HCRefs.MOD_ID)
